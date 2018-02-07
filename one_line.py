242062275b723c6cf54136ed80a40001802664f4
Neil Niu <newbon@gmail.com>
2018-02-07 14:35:52 +0800

    to_be_condensed

--- one_line.py
+++ one_line.py
@@ -1,6 +1,25 @@
-#!usr/bin/python3
+#!usr//bin//python3
 #-*-coding:utf-8-*-
 
 #[XNiu2 2018-02-07]
 
+# def palindrome(n, c):
+#     h = n//2
+#     if h != 0:
+#         if n % 2 == 0:
+#             new_c = (h//len(c))*c + c[0:(h%len(c))]
+#             ans = new_c + new_c[::-1]
+#         else:
+#             new_c = c[0:(len(c)-1)]
+#             half_len = len(new_c)
+#             new_c = (h//half_len)*new_c + new_c[h%half_len]
+#             ans = new_c + c[-1] + new_c[::-1]
+#     else:
+#         return c
+#     return ans
+
 def palindrome(n, c):
+    h = n//2
+    new_c = (h//len(c)) * c + c[0:(h%len(c))]
+    return (new_c+new_c[::-1]) if (n%2 == 0) else (new_c+c[-1]+new_c[::-1])
+print (palindrome(1, "a"))
