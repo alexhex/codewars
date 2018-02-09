02579b87ca4ffb3d253487e4101753744beb83a6
Neil Niu <newbon@gmail.com>
2018-02-09 10:23:59 +0800

    [XNiu2 09-Feb-2018] Practice

---
+++ coloured_triangles.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3 
+#-*conding:utf-8-*-
+
+# updated by XNiu2 on 09-Feb-2018
+
+def puke(row):
+    al = 'RGB'
+    if row[0] == row[1]:
+        return row[0]
+    else:
+        al = al.replace(row[0],'').replace(row[1], '')
+    return al
+    
+def triangle(row):
+    if (len(row) == 1):
+        return row 
+    ans = ''
+    while True: 
+        for i in range(0,len(row)-1):
+            ans += puke(row[i:i+2])
+        if (len(ans) == 1):
+            break
+        row = ans
+        ans = ''
+    return ans
+
+print (triangle('R'))
